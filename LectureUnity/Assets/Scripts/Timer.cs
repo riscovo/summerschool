c4db4c8e003b951fa2b30523ddf1c5940cd78d64
Miletti67 <miletti67@gmail.com>
2019-08-13 16:17:31 +0200

    Entire Unity project so far

---
+++ LectureUnity/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float targetTime = 60.0f;
+
+    Text instruction;
+
+    // Use this for initialization
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+        
+    }
+
+    void Update()
+    {
+
+        targetTime -= Time.deltaTime;
+        instruction.text = ((int)targetTime).ToString();
+
+    }
+
+
+}
