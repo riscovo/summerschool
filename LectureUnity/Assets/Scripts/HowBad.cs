df036488be5a9fb1875a976567b9e2bb3b6d5024
Miletti67 <miletti67@gmail.com>
2019-08-14 17:14:05 +0200

    Street Assets

--- LectureUnity/Assets/Scripts/HowBad.cs
+++ LectureUnity/Assets/Scripts/HowBad.cs
@@ -25,7 +25,13 @@ public class HowBad : MonoBehaviour
             switch (good)
             {
                 case 0:
-                    bad.text = "Let me guess: you don't really like old people.";
+                    bad.text = "Everyone is shocked by the horrific accident but while they try to act sympathetic towards your tragic end, in" +
+                        "  their homes some people whisper about you as a 'racist pig', better of dead. Your neighbour, devastated by the loss of" +
+                        "his two children drinks himself to the egde of his sanity. In a vengeful act he sets your empty house on fire, accidently" +
+                        " burning his own house down as well, with his wife still inside. There is a funeral for you and little Timmy, half of the" +
+                        " town having come to say goodbye. Your daughter is sitting in the front row, tears streaming down her cheeks, the social" +
+                        " worker waiting by the door for her. She will spend the rest of her youth in an orphanage and have become a prostitue by" +
+                        " the age of 18, eventually dying of AIDS two years later.";
                     break;
                 case 1:
                     bad.text = "What did those old ladys ever do to you?!";
@@ -40,7 +46,14 @@ public class HowBad : MonoBehaviour
                     bad.text = "Apparently that one lady was just really slow.";
                     break;
                 case 5:
-                    bad.text = "You have something against guys, don't you?";
+                    bad.text = "You are devastasted by the events. The whole town looks down upon you and you are despised by the strong LGBTQ" +
+                        " community in the city, hated by otheres. Your co-workers call you a 'homophobic caveman'. NO one cares about the future" +
+                        " of Suntown anymore, no one protests against Chemotox anymore so they will build their facility, polluting the groundwater" +
+                        " and the soil in the process. This will result in the forced eviction of the whole Suntown leaving it completely abandoned" +
+                        " by the time four years have passed. You're opening a sad chapter of your life. There is a picture of your daughter hanging" +
+                        " above the fireplace, forever reminding you of your last words to her and leaving you hopeless and miserable with the knowledge" +
+                        " that some things can not be undone. So you pour all your sadness and anger into your growing baby, who will grow up to be an" +
+                        " irresponsible punk, only to die at the age of 17 by a drug overdose.";
                     break;
 
             }
